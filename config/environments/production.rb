932c41b1bdc466b609c118ef0241365a8647bbf2
azaupa <andrea.zaupa@develon.com>
2013-09-06 16:19:52 +0200

    asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Lobsters::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
 
+  config.action_mailer.default_url_options = { host: 'develon.hack-inter.net' }
+  config.action_mailer.asset_host = 'http://develon.hack-inter.net'
+
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
